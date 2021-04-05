d58f662d29577ee88e8c3a5c9a9fcf5f02d13edb
TheSowrdBreaker <kunalguptadev007@gmail.com>
2021-04-05 15:27:32 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            countasdsa is: {count}
+          </button>
+        </p>
+        dadas
+        <p>
+          Edit <code>App.tsx</code> and save to testajejahjdh test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer">
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

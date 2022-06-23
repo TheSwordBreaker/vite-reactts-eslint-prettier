b1874aac08d367d7597170dd602e05a0fec33816
The Sword Breaker <52120562+TheSwordBreaker@users.noreply.github.com>
2022-06-23 18:42:57 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react()],
 });

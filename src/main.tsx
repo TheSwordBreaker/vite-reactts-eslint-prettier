1deeb562b455d67b4f08ce4c1556dd90f3176ed3
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-12-22 08:48:37 +0100

    modified main.tsx with new guidelines

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import './css/index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root') ?? document.createElement('div');
+const root = createRoot(rootElement!);
+
+root.render(<App />);
